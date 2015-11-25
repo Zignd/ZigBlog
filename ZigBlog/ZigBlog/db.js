74aba0a5cd1d642a3379ee860e44ae4296b69b74
Igor Hipólito Vieira <zignd.igor@gmail.com>
2015-11-25 10:43:39 -0200

    Added a collection that was missing in the database creation file

--- ZigBlog/ZigBlog/db.js
+++ ZigBlog/ZigBlog/db.js
@@ -1,5 +1,7 @@
 ﻿db.createCollection('Parameters');
 
+db.createCollection('Posts');
+
 db.createCollection('Users');
 db.Users.createIndex({ UserNameLower: 1 }, { unique: true });
 db.Users.createIndex({ EmailAddress: 1 }, { unique: true, sparse: true });
